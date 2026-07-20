ed1bc2344f6350aa8a6276e26da5b810342ea289
PRASHANT RAJESH SUTHAR <prashantsuthar.1802@gmail.com>
2026-07-20 21:22:02 +0530

    Solved on 20-07-2026

--- 62.unique-paths.cpp
+++ 62.unique-paths.cpp
@@ -3,12 +3,22 @@
  *
  * [62] Unique Paths
  */
+#include <vector>
+using namespace std;
 
 // @lc code=start
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
 // @lc code=end
