176e69f9f6fe2bd6853770e421bdd32c4d27c1ee
PRASHANT RAJESH SUTHAR <prashantsuthar.1802@gmail.com>
2026-07-25 23:35:36 +0530

    Solved on 25-07-2026

---
+++ 113.path-sum-ii.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=113 lang=cpp
+ *
+ * [113] Path Sum II
+ */
+
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void dfs(TreeNode *root, int targetSum)
+    {
+        if (root == nullptr)
+            return;
+
+        path.push_back(root->val);
+        targetSum -= root->val;
+
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            if (targetSum == 0)
+                ans.push_back(path);
+        }
+        else
+        {
+            dfs(root->left, targetSum);
+            dfs(root->right, targetSum);
+        }
+
+        path.pop_back(); // Backtrack
+    }
+
+    vector<vector<int>> pathSum(TreeNode *root, int targetSum)
+    {
+        dfs(root, targetSum);
+        return ans;
+    }
+};
+// @lc code=end
